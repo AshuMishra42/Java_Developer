98f26395d271862b192edb6d803c8280abcbaa20
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-09-04 20:30:27 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,15 @@
+// PRINT STARS IN A REPEATED MANNER :
+
+
+public class second {
+    public static void main(String[] args) {
+    //    System.out.println("*");
+    //    System.out.println("**");        
+     //  System.out.println("***");        
+     //   System.out.println("****");
+        
+     System.out.print("*\n**\n***\n****\n*****"+" ");
+    }
+}
+
+// Same Code Implemented by another way

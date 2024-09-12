6abb373313b5f6a101b9da05e688390198ff4941
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-09-12 18:57:51 +0530

    Add files via upload

---
+++ fourth.java
@@ -0,0 +1,12 @@
+// WAP TO PRINT A NAME BY USING TAKING USER INPUT 
+
+import java.util.*;
+
+public class fourth {
+    public static void main(String[] args) {
+        System.out.print("Enter the name of Student= ");
+        Scanner obj= new Scanner(System.in);
+        String name= obj.next();
+        System.out.println(name);
+    }
+}

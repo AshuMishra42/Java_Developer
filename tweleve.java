413ef0ae98a0ca658b56cf2356ed5296ad3d10cc
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-09-20 10:22:05 +0530

    Add files via upload

---
+++ tweleve.java
@@ -0,0 +1,10 @@
+// Print numbers between from 1 to 100 by using Loops 
+
+
+public class tweleve {
+    public static void main(String[] args) {
+        for(int counter=100; counter>=1; counter--){
+            System.out.println(counter);
+        }
+    }
+}

ef213d8ddb181886faf19d0ae08a72d60432b578
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-09-20 10:21:29 +0530

    Add files via upload

---
+++ eleventh.java
@@ -0,0 +1,10 @@
+// Print statement with the help  of loops :
+
+
+public class eleventh {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println("Hii My Name is Ankush Kumar Mishra");
+        }
+    }
+}
